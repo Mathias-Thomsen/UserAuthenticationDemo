93bddf7ce6c5dd29911ab05483d0b3e154feb592
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2024-03-21 21:46:52 +0100

    Added user authentication and personal collection with notes

--- App.js
+++ App.js
@@ -1,11 +1,156 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Platform } from 'react-native';
+import { useState, useEffect } from 'react';
+import { app, database, auth } from './firebase/firebaseConfig'
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword, signOut } from 'firebase/auth'
+
 
 export default function App() {
+  const [enteredEmail, setEnteredEmail] = useState("mathias@test.dk");
+  const [enteredPassword, setEnteredPassword] = useState("123456");
+  const [userId, setUserId] = useState(null);
+  const [enteredText, setEnteredText] = useState("");
+  const [documents, setDocuments] = useState([])
+ 
+
+  useEffect(() => {
+    
+    const auth_ = getAuth()
+    const unsubcribe = onAuthStateChanged(auth_, (currentUser) => {
+      if(currentUser) {
+        setUserId(currentUser.uid)
+        fetchDocuments()
+      } else {
+        setUserId(null)
+      }
+    })
+    return () => unsubcribe() //kaldes når componenten unmountes
+  }, [])
+
+
+  
+  
+
+  async function addDocument() {
+    if (!enteredText.trim()) {
+      alert("Teksten kan ikke være tom!")
+      console.log("Teksten kan ikke være tom.");
+      return; // Afslutter funktionen tidligt, hvis betingelsen er sand
+    }
+    try {
+      // Tilføjer dokumentet til Firestore og gemmer referencen
+      const docRef = await addDoc(collection(database, userId), {
+        text: enteredText
+      });
+      
+      // Opdaterer local state med det nye dokument
+      // Bemærk, at vi bruger docRef.id til at få ID'et for det tilføjede dokument
+      setDocuments(prevDocuments => [...prevDocuments, { id: docRef.id, text: enteredText }]);
+      
+      // Nulstil indtastningsfeltet, hvis det ønskes
+      setEnteredText("");
+    } catch (error) {
+      console.log("error addDocument " + error);
+    }
+  }
+
+  async function fetchDocuments() {
+    if (userId) {
+      const querySnapshot = await getDocs(collection(database, userId));
+      const docsArray = [];
+      querySnapshot.forEach((doc) => {
+        docsArray.push({ id: doc.id, ...doc.data() });
+      });
+      setDocuments(docsArray);
+    }
+  }
+
+  async function login() {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, enteredEmail, enteredPassword);
+      setUserId(userCredential.user.uid); 
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  }
+
+  async function signup() {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, enteredEmail, enteredPassword);
+      setUserId(userCredential.user.uid); 
+    } catch (error) {
+      console.error("Signup error:", error);
+    }
+  }
+
+  async function sign_out() {
+    await signOut(auth)
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      { !userId &&
+        <>
+          <Text>Login</Text>
+          <TextInput
+            onChangeText={newText => setEnteredEmail(newText)}
+            value={enteredEmail}
+          />
+          <TextInput
+            onChangeText={newText => setEnteredPassword(newText)}
+            value={enteredPassword}
+          />
+          <Button
+            title='Log in'
+            onPress={login}
+          />
+
+          <TextInput
+            onChangeText={newText => setEnteredEmail(newText)}
+            value={enteredEmail}
+          />
+          <TextInput
+            onChangeText={newText => setEnteredPassword(newText)}
+            value={enteredPassword}
+          />
+          <Button
+            title='Signup'
+            onPress={signup}
+          />
+        </>
+      }
+      {userId && documents.length > 0 && (
+        <>
+          {documents.map((document) => (
+            <View key={document.id} style={{margin: 10}}>
+              <Text>{document.text}</Text>
+            </View>
+          ))}
+        </>
+      )}
+
+      { userId &&
+        <>
+          <TextInput
+            placeholder="type here" 
+            onChangeText={newText => setEnteredText(newText)}
+            value={enteredText}
+          />
+          <Button
+            title='Add new Document'
+            onPress={addDocument}
+          />
+          <Button
+            title='Sign out'
+            onPress={sign_out}
+          />
+          </>
+      }
+      
+      
     </View>
   );
 }
---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,50 @@
+// Importer nødvendige Firebase moduler
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, initializeAuth } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {getReactNativePersistence } from 'firebase/auth';
+import { Platform } from 'react-native';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyClmwnUppm7Y2An1SNNsG4OXsX8kgVlI9Q",
+
+  authDomain: "userauthenticationdemo-79887.firebaseapp.com",
+
+  projectId: "userauthenticationdemo-79887",
+
+  storageBucket: "userauthenticationdemo-79887.appspot.com",
+
+  messagingSenderId: "871397763524",
+
+  appId: "1:871397763524:web:8a1b9f4da23a118c621c91"
+
+};
+
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialiser Firestore
+const database = getFirestore(app);
+
+
+// Initialiser Firebase Auth baseret på platform
+let auth;
+if (Platform.OS === 'web') {
+  auth = getAuth(app);
+} else {
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  });
+}
+
+export { app, database, auth };
